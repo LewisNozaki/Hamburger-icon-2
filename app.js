beba377bfaa5302129f33f43a320aad78392d8d4
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-04 21:37:12 -1000

    click animation needs some work

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 "use strict";
 
 const container = document.getElementsByClassName("container")[0];
+let one = document.getElementsByClassName("one")[0];
+let two = document.getElementsByClassName("two")[0];
+let three = document.getElementsByClassName("three")[0];
 
 const moveLines = () => {
-  console.log("clicked");
+  one.classList.add("left-side");
+  two.classList.add("make-big");
+  three.classList.add("right-side");
 };
 
 container.addEventListener("click", moveLines);
+
+const createDots = () => {
+  one.classList.add("moveOne");
+  two.classList.add("moveTwo");
+  three.classList.add("moveThree");
+}
+
+container.addEventListener("mouseover", createDots);
