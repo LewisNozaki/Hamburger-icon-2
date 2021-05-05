fadae8a7f627c91d4dbfd34cff1e8234b1d80a83
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-04 20:16:34 -1000

    event listener

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const container = document.getElementsByClassName("container")[0];
+
+const moveLines = () => {
+  console.log("clicked");
+};
+
+container.addEventListener("click", moveLines);
